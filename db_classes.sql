dff88ec5cbce97ab761eb85741dbe3aa6b68c522
Brooks Radtke <brooks@brooksradtke.com>
2020-12-17 11:08:23 -0800

    Push Changes to Repository

--- db_classes.sql
+++ db_classes.sql
@@ -16,8 +16,39 @@ CREATE TABLE tbl_instructors
 	INSTRUCTOR_NAME VARCHAR(50) NOT NULL);
 
 ALTER TABLE tbl_students
-ADD CLASS_ID VARCHAR(50), INSTRUCTOR_ID VARCHAR(50);
+ADD CLASS_ID INT, INSTRUCTOR_ID INT;
 
 ALTER TABLE tbl_students
 ADD CONSTRAINT fk_CLASS_ID
-FOREIGN KEY REFERENCES tbl_classes(CLASS_ID);
+FOREIGN KEY (CLASS_ID) REFERENCES tbl_classes(CLASS_ID);
+
+INSERT INTO tbl_classes 
+	VALUES
+	('Software Developer Boot Camp'),
+	('C# Boot Camp');
+
+INSERT INTO tbl_students (STUDENT_NAME)
+	VALUES
+	('Sean C'),
+	('Joe O'),
+	('Greg H'),
+	('Josh W'),
+	('Mariah Y'),
+	('Ted T');
+
+INSERT INTO tbl_instructors
+	VALUES
+	('Erik Gross'),
+	('Sir Ulrich Von Liechtenstien');
+
+--step 9
+UPDATE tbl_students
+SET STUDENT_NAME = 'Sean C'
+WHERE CLASS_ID = 100;
+
+SELECT * FROM tbl_students
+SELECT * FROM tbl_classes
+
+UPDATE tbl_students
+SET STUDENT_NAME = 'Sean C'
+WHERE INSTRUCTOR_ID = 'Erik Gross';
